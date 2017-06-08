8d2843807f7f758ad529bc471e682cb32674f2b3
Alex Ewerlöf <userpixel@users.noreply.github.com>
2017-06-08 23:54:27 +0200

    introduced partials to modularize html and share across pages (#19)
    * introduced partials to modularize html and share across pages
    
    * fixed a bug with passing configs to js

---
+++ public/index.js
@@ -0,0 +1,51 @@
+/* global SPiD, $ */
+/* eslint-env browser */
+
+'use strict';
+
+// This stuff will ofc end up in the JS SDK
+$(document).ready(function() {
+    // TODO remove this Workaround
+    SPiD.Talk = {
+        response: (id, data) => window[id](data)
+    };
+
+    const spid = new SPiD.default({
+        serverUrl: window.config.spidBaseUrl,
+        popup: true,
+        useSessionCluster: false,
+        client_id: window.config.clientId,
+        redirect_uri: window.location.toString()
+    });
+
+    const safePage = `${window.location.origin}/safepage`;
+
+    spid.event.addListener('SPiD.logout', function () {
+        alert('Logged out from SPiD. Need to cancel out session');
+    });
+
+    // Check session
+    spid.hasSession();
+
+    $('#login-user-pass').click(function (e) {
+        e.preventDefault();
+        spid.login(safePage);
+    });
+
+    $('#login-email').click(function (e) {
+        e.preventDefault();
+        spid.login(safePage, 'otp-email');
+    });
+
+    $('#login-sms').click(function (e) {
+        e.preventDefault();
+        spid.login(safePage, 'otp-sms');
+    });
+
+    $('#logout-btn').click(function () {
+        // TEMPORARY HACK UNTIL LOGOUT WORKS IN THE browser-sdk API
+        $.ajax('/session', { method: 'DELETE' })
+            .then(() => alert('Logged out!'))
+            .catch((err) => alert(`Could not log out! ${err}`));
+    });
+});
